c3ad8416da8962b755a8bf085cdcd73532c08566
PeerDino <20cs064@students.muet.edu.pk>
2023-07-20 14:10:10 +0500

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,41 @@
 const app = require("./app");
 const connectDatabase = require("./config/database");
-// const dotenv = require("dotenv");
 const cloudinary = require("cloudinary");
+const dotenv = require("dotenv");
 const PORT = process.env.PORT || 4000;
-// handle the uncaught exceptions
+
+// Handle uncaught exceptions
 process.on("uncaughtException", (err) => {
-  console.log(`ERROR:${err.stack}`);
+  console.log(`ERROR: ${err.stack}`);
   console.log("Shutting down server due to uncaught exception");
   process.exit(1);
 });
-//setting up the config file
-// if (process.env.NODE_ENV !== "PRODUCTION")
-require("dotenv").config({ path: "backend/config/config.env" });
-// dotenv.config({ path: "backend/config/config.env" });
 
-// connecting to databse
+// Load environment variables
+dotenv.config({ path: "backend/config/config.env" });
+
+// Connect to the database
 connectDatabase();
 
-// Setting up config file
+// Set up Cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// Start the server
 const server = app.listen(PORT, () => {
-  console.log(`server started on PORT :${PORT} in
-    ${process.env.NODE_ENV} mode.`);
+  console.log(
+    `Server started on PORT: ${PORT} in ${process.env.NODE_ENV} mode.`
+  );
 });
 
-// handle unhandled promise rejections
-process.on("unhandleRejection", (err) => {
-  console.log(`ERROR:${err.stack}`);
-  console.log("shutting down the server due to unhandled Promise rejection");
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down the server due to unhandled Promise rejection");
   server.close(() => {
-    process.exixt(1);
+    process.exit(1);
   });
 });
